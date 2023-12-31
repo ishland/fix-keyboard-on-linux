3d32d3cdf04737204fc5f14c057701fb58777ee0
ishland <ishlandmc@yeah.net>
2023-12-31 22:27:59 +0800

    change: only detect left alt key
    Related to #2

--- src/main/java/com/ishland/fixkeyboardonlinux/mixin/MixinKeyboard.java
+++ src/main/java/com/ishland/fixkeyboardonlinux/mixin/MixinKeyboard.java
@@ -1,7 +1,10 @@
 package com.ishland.fixkeyboardonlinux.mixin;
 
 import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +15,7 @@ public class MixinKeyboard {
 
     @Inject(method = "onChar", at = @At(value = "HEAD"), cancellable = true)
     private void beforeOnChar(CallbackInfo ci) {
-        if (Screen.hasControlDown() || Screen.hasAltDown()) ci.cancel();
+        if (Screen.hasControlDown() || InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_ALT)) ci.cancel();
     }
 
 }
